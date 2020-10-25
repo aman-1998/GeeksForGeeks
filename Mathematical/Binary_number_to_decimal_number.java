1d0c8cc62d604fb4182c747b505eda911b27173d
aman-1998 <mishraamankpa@gmail.com>
2020-10-25 22:53:38 +0530

    Topic : Mathematical

--- Mathematical/Binary_number_to_decimal_number.java
+++ Mathematical/Binary_number_to_decimal_number.java
@@ -15,7 +15,8 @@ Output: 44
  
 
 Your Task:
-You don't need to read or print anything. Your task is to complete the function binary_to_decimal() which takes the binary number as string input parameter and returns its decimal equivalent.
+You don't need to read or print anything. Your task is to complete the function binary_to_decimal() 
+which takes the binary number as string input parameter and returns its decimal equivalent.
  
 
 Expected Time Complexity: O(K * Log(K)) where K is number of bits in binary number.
